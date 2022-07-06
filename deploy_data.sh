8c3435a24aac9d1a0875d729a43fbd3ed3415734
wangxf2000 <wangxf.2000@gmail.com>
2022-07-06 14:15:15 +0800

    Update deploy_data.sh

--- deploy_data.sh
+++ deploy_data.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
-echo "-- Commencing SingleNodeCluster Setup Script"
+echo "-- Commencing Car insurance data Setup Script"
 
 set -e
 set -u
 
+echo "-- deploy mock data in HDFS"
 export HADOOP_USER_NAME=hdfs
 hdfs dfs -mkdir -p /tmp/customers
 hdfs dfs -mkdir -p /tmp/claims
 hdfs dfs -chown -R impala:hadoop /tmp/customers
 hdfs dfs -chown -R impala:hadoop /tmp/claims
 export HADOOP_USER_NAME=impala
-hdfs dfs -put ~/car_insurance/customers.csv /tmp/customers
-hdfs dfs -put ~/car_insurance/claims.csv /tmp/claims
+hdfs dfs -put customers.csv /tmp/customers
+hdfs dfs -put claims.csv /tmp/claims
 
+echo "-- Create kudu tables and initial data"
 impala-shell -f ~/car_insurance/create_table.sql -B -o ~/car_insurance/create_table.sql.log
+
+echo "-- Create kudu tables and initial data completely"
+
