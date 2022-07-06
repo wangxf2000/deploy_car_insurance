950544e42bd614ce7f36e58ed59c69458906b269
wangxf2000 <wangxf.2000@gmail.com>
2022-07-06 14:52:43 +0800

    Update deploy_model.sh

--- deploy_model.sh
+++ deploy_model.sh
@@ -10,13 +10,13 @@ yum install -y centos-release-scl
 yum install -y rh-python36 
 pip install requests
 
-echo "--Download the models to /tmp/resources"
+echo "--Download the models to /tmp/resource"
 ##Download four models
-mkdir /tmp/resources
-wget https://github.com/bguedes/carDetectionPredictionML/blob/main/models/car_model_cat_list.pk -P /tmp/resources 
-wget https://github.com/bguedes/carDamagePredictionML/blob/main/models/carDamagePredictionModel.h5  -P /tmp/resources
-wget https://github.com/bguedes/carDamageLocalizationPredictionML/blob/main/models/carDamageLocalizationPredictionModel.h5 -P /tmp/resources
-wget https://github.com/bguedes/carDamageSeverityPredictionML/blob/main/models/carDamageSeverityPredictionModel.h5 -P /tmp/resources
+mkdir /tmp/resource
+wget https://github.com/bguedes/carDetectionPredictionML/blob/main/models/car_model_cat_list.pk -P /tmp/resource 
+wget https://github.com/bguedes/carDamagePredictionML/blob/main/models/carDamagePredictionModel.h5  -P /tmp/resource
+wget https://github.com/bguedes/carDamageLocalizationPredictionML/blob/main/models/carDamageLocalizationPredictionModel.h5 -P /tmp/resource
+wget https://github.com/bguedes/carDamageSeverityPredictionML/blob/main/models/carDamageSeverityPredictionModel.h5 -P /tmp/resource
 
 echo "--Config THE_PWD env"
 export THE_PWD=admin
@@ -31,16 +31,16 @@ source /opt/rh/rh-python36/enable
 export THE_PWD=admin
 echo "--deploy the cdsw project and models"
 echo "--deploy carPictureDetection_cdsw_setup..."
-python -u setup_model/carPictureDetection_cdsw_setup.py $PUBLIC_IP /tmp/resources/car_model_cat_list.pk
+python -u setup_model/carPictureDetection_cdsw_setup.py $PUBLIC_IP /tmp/resource/car_model_cat_list.pk
 echo "--deploy carPictureDetection_cdsw_setup completely"
 echo "--deploy carDamagePrediction_cdsw_setup..."
-python -u setup_model/carDamagePrediction_cdsw_setup.py $PUBLIC_IP /tmp/resources/carDamagePredictionModel.h5
+python -u setup_model/carDamagePrediction_cdsw_setup.py $PUBLIC_IP /tmp/resource/carDamagePredictionModel.h5
 echo "--deploy carDamagePrediction_cdsw_setup completely"
 echo "--deploy carDamageLocalization_cdsw_setup..."
-python -u setup_model/carDamageLocalization_cdsw_setup.py $PUBLIC_IP /tmp/resources/carDamageLocalizationPredictionModel.h5
+python -u setup_model/carDamageLocalization_cdsw_setup.py $PUBLIC_IP /tmp/resource/carDamageLocalizationPredictionModel.h5
 echo "--deploy carDamageLocalization_cdsw_setup completely"
 echo "--deploy carDamageSeverity_cdsw_setup..."
-python -u setup_model/carDamageSeverity_cdsw_setup.py $PUBLIC_IP /tmp/resources/carDamageSeverityPredictionModel.h5
+python -u setup_model/carDamageSeverity_cdsw_setup.py $PUBLIC_IP /tmp/resource/carDamageSeverityPredictionModel.h5
 echo "--deploy carDamageSeverity_cdsw_setup completely"
 echo "--Deploy all cdsw models completely"
 
