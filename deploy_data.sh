30db29b67168a57cbf62bcdd62a53948e331a353
wangxf2000 <wangxf.2000@gmail.com>
2022-07-11 15:56:59 +0800

    Update deploy_data.sh

--- deploy_data.sh
+++ deploy_data.sh
@@ -12,11 +12,11 @@ hdfs dfs -mkdir -p /tmp/claims
 hdfs dfs -chown -R impala:hadoop /tmp/customers
 hdfs dfs -chown -R impala:hadoop /tmp/claims
 export HADOOP_USER_NAME=impala
-hdfs dfs -put customers.csv /tmp/customers
-hdfs dfs -put claims.csv /tmp/claims
+hdfs dfs -put data/customers.csv /tmp/customers
+hdfs dfs -put data/claims.csv /tmp/claims
 
 echo "-- Create kudu tables and initial data"
-impala-shell -f ~/car_insurance/create_table.sql -B -o ~/car_insurance/create_table.sql.log
+impala-shell -f create_table.sql -B -o create_table.sql.log
 
 echo "-- Create kudu tables and initial data completely"
 
