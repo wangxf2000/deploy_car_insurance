b5a3bf8633f7f68b87b2d9b2bd1e527f3ca53e8d
wangxf2000 <wangxf.2000@gmail.com>
2022-07-19 15:44:36 +0800

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+echo "-- Commencing Car insurance cdsw model Setup Script"
+
+set -e
+set -u
+
+echo "-- install dependency packages"
+yum -y remove epel-release
+yum install -y centos-release-scl epel-release
+yum install -y rh-python36 python-pip
+pip install --upgrade pip==19.3
+pip install requests
+BASE_DIR="/tmp/resource"
+
+echo "--Download the models to ${BASE_DIR}"
+##Download four models
+if [ -d "${BASE_DIR}" ]; then
+  mkdir -p ${BASE_DIR}
+fi
+git clone https://github.com/bguedes/carDetectionPredictionML
+git clone https://github.com/bguedes/carDamagePredictionML
+git clone https://github.com/bguedes/carDamageLocalizationPredictionML
+git clone https://github.com/bguedes/carDamageSeverityPredictionML
+cp carDetectionPredictionML/models/car_model_cat_list.pk  ${BASE_DIR} 
+cp carDamagePredictionML/models/carDamagePredictionModel.h5   ${BASE_DIR}
+cp carDamageLocalizationPredictionML/models/carDamageLocalizationPredictionModel.h5  ${BASE_DIR}
+cp carDamageSeverityPredictionML/models/carDamageSeverityPredictionModel.h5  ${BASE_DIR}
+#wget https://github.com/bguedes/carDetectionPredictionML/blob/main/models/car_model_cat_list.pk -P ${BASE_DIR} 
+#wget https://github.com/bguedes/carDamagePredictionML/blob/main/models/carDamagePredictionModel.h5  -P ${BASE_DIR}
+#wget https://github.com/bguedes/carDamageLocalizationPredictionML/blob/main/models/carDamageLocalizationPredictionModel.h5 -P ${BASE_DIR}
+#wget https://github.com/bguedes/carDamageSeverityPredictionML/blob/main/models/carDamageSeverityPredictionModel.h5 -P ${BASE_DIR}
+
+echo "--Config THE_PWD env"
+echo -n "supersecret1" > ${BASE_DIR}/the_pwd.txt
